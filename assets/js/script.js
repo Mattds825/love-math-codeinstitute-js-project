cd44d736b0f7d015d640e8e1a770daf369710d21
Matthew Da Silva <matthewgdasilva25@gmail.com>
2024-08-20 14:48:00 +0100

    display addition question to user on page load and addition game button click

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,19 +9,29 @@ document.addEventListener("DOMContentLoaded", function(){
                 alert("you've clicked submit");
             } else{
                 let gameType = this.getAttribute("data-type");
-                alert(`you've clicked ${gameType}`);
+                runGame(gameType);
             }
         });
     }
+
+    runGame("addition");
 });
 /**
  * The main game "loop", called when the script is first loaded
+ * accept a gameType
  * and after the user's answer has been processed
  */
-function runGame(){
+function runGame(gameType){
     // create random numbers between 1 and 25
     let num1 = Math.floor(Math.random()*25)+1;
     let num2 = Math.floor(Math.random()*25)+1;
+
+    if (gameType === "addition"){
+        displayAdditionQuestion(num1, num2);
+    } else{
+        alert(`unknown game type!: ${gameType}`);
+        throw(`unknown game type: ${gameType}, Aborting!`);
+    }
 }
 
 
@@ -41,7 +51,10 @@ function incrementWrongAnswer(){
 
 }
 
-function displayAdditionQuestion(){
+function displayAdditionQuestion(operand1, operand2){
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
 
 }
 
